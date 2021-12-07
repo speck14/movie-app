819f3f91a4d79541c4f3175cdbed9f2440e45b12
Shannon Peck <shannon.peck@cornerstonenw.com>
2021-12-07 12:13:13 -0800

    wrapped fetch in React.useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 function App() {
-  fetch('http://localhost:5000/express_backend')
-  .then(response => response.json())
-  .then(data => console.log(data));
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("http://localhost:5000/express_backend")
+      .then((res) => res.json())
+      .then(data => setData(data.express));
+  }, []);
+
   return (
-  <div className="App">
-  app
-  </div>
+    <div className="App">
+      <h1 className="App-heading">Hello from React!</h1>
+      <p>{!data ? "Loading..." : data}</p>
+    </div>
   );
-  }
+}
 export default App;
