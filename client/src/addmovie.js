4f482c9e0b4b110673dee2d7b33b335f5edfd708
Shannon Peck <shannon.peck@cornerstonenw.com>
2021-12-29 16:21:31 -0800

    started 'addMovie' form, refactored movieitem.js

--- client/src/addmovie.js
+++ client/src/addmovie.js
@@ -1,9 +1,36 @@
 import React, {useState, useEffect} from 'react';
 
 function AddMovie() {
+  const [submitting, setSubmitting] = useState(false);
+
+const handleSubmit = event => {
+  event.preventDefault();
+  setSubmitting(true);
+
+  setTimeout(() => {
+    setSubmitting(false);
+  }, 2000)
+};
+
   return(
     <div>
-      <h2>This page will add a movie</h2>
+      <h2>Add a movie:</h2>
+      {submitting &&
+      <div>Submitting Form...</div>
+      }
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <div>
+          <label for="movieName">Movie Title:</label>
+          <input type="text" id="movieName" placeholder="Movie Title"></input>
+          </div>
+          <div>
+            <label for="genre">Genre(s):</label>
+            <input type="genre" id="genre" placeholder="Genre"></input>
+          </div>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 };
--- client/src/movieitem.js
+++ client/src/movieitem.js
@@ -1,23 +1,3 @@
-/* 
-1)useEffect is called on render (re-render triggered when match.params.pk changes)
-2) useEffect triggers a call to getMovieInfo, which does a get request to /genres/pk
-  -Get movie name and genre_fks from JSON response, set movieItem state to that data via setMovieItem
-3)getMovieInfo then calls getCurrentMovieGenres
-  -Makes a get request to /genres to get all genre information
-  ****
-  -sets allGenres to JSON response via setAllGenres
-    -This is where the app isn't working. The request is successful, the JSON response contains genre data,
-      but the setAllGenres call isn't setting allGenres to the JSON data
-    -The setAllGenres works if I'm already on the movieItemView page and restart the app, it doesn't work when
-      navigating from the movieView page via the link, and it doesn't work if the the page is refreshed
-    -Calls are happening in the correct order, so I don't *think* it's an async/call stack order problem
-      ****
-4) getAllGenres then calls getCurrentMovieGenres
-  -This uses the genre_fks from movieItem, compares it to pks for all genres, and adds matching pk/fks to
-    movieGenres via setGenres
-5)Display movie name and a list of genres for current movie
-*/
-
 import React, { useState, useEffect } from "react";
 //import { Link } from "react-router-dom";
 
@@ -30,22 +10,27 @@ var MovieInfo = ({ name }) => (
 var DisplayGenres = ({ genreName }) => <li>{genreName}</li>;
 
 function MovieItemView({ match }) {
-  var initialMovieViewState = {
+  //Would useReducer be better than useState to capture initial state?
+var initialMovieViewState = {
     pk: "",
     name: "",
     genre_fks: [],
   };
   var [movieItem, setMovieItem] = useState(initialMovieViewState); //obj, key: genre_fks
-  var [movieGenres, setGenres] = useState([]);
-  var [allGenres, setAllGenres] = useState([]);
+  var [currentMovieGenres, setGenres] = useState([]);  
+  //var [allGenres, setAllGenres] = useState([]);
 
-  function getCurrentMovieGenres() {
+  async function getCurrentMovieGenres(allGenres) {
     console.log(`AllGenres: ${allGenres}`);
     console.log(`MovieItem: ${movieItem}`);
-    var currentMovieGenres = allGenres.filter((genre) => {
+    var movieGenres = allGenres.filter((genre) => {
       return movieItem.genre_fks.includes(genre.pk);
-    });
-    setGenres(currentMovieGenres);
+    }); //if the genre is in moveItem's genre array, add to currentMovieGenres
+/*Currently, I think this is using the initialMovieViewState on the first rendering (not the movieItem fetched in getMovieInfo)
+    -Based on the fact that if I don't initialize initialMovieViewState, I get a "Cannot read properties of undefined (reading'includes')" 
+    -Would useReducer (or another hook) fix that?
+*/
+    setGenres(movieGenres);
   };
 
   async function getAllGenres() {
@@ -53,9 +38,7 @@ function MovieItemView({ match }) {
       var res = await fetch(`http://localhost:5000/genres`);
       var data = await res.json();
       console.log(data);
-      await setAllGenres([...data]);
-      console.log(`getAllGenres ran, allGenres: ${allGenres}`);
-      getCurrentMovieGenres();
+      getCurrentMovieGenres(data);
     } catch (e) {
       console.error(e);
     }
@@ -66,15 +49,18 @@ function MovieItemView({ match }) {
       var res = await fetch(`http://localhost:5000/movies/${pk}`);
       var data = await res.json();
       setMovieItem(data);
-      await getAllGenres();
+      //await getAllGenres();
     } catch (e) {
       console.error(e);
     }
   };
 
   useEffect(() => {
-    getMovieInfo(match.params.pk).catch((e) => console.error(e));
-  }, [match.params.pk]);
+    getMovieInfo(match.params.pk)
+    .then(getAllGenres())
+    .catch((e)=> console.error(e));
+
+  }, [match]);
 
   return (
     <div>
@@ -82,7 +68,7 @@ function MovieItemView({ match }) {
       <MovieInfo name={`${movieItem.name}`} />
       <h4>Genres:</h4>
       <ul>
-        {movieGenres.map((genre) => (
+        {currentMovieGenres.map((genre) => (
           <DisplayGenres key={`${genre.pk}`} genreName={`${genre.name}`} />
         ))}
       </ul>
