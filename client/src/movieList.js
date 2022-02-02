5855de90179b1280ab1c297e8de693edc2b99a7f
Shannon Peck <shannon.peck@cornerstonenw.com>
2022-02-02 14:05:49 -0800

    completed abstraction of 'Movie' from movieList

--- client/src/movieList.js
+++ client/src/movieList.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-//list items for individual movies
-var Movie = ({ name }) => <li>{name}</li>;
+/*
+  I wanted the user to "feel" like they're being directed to individual movie's page, but under the hood React 
+  is conditionally rendering the movie view component, not directing the user somewhere.
+              
+  I used button's ability to perform an action, with CSS styling to make the button look like a link instead.
+*/
+var Movie = ({ clickHandler, name }) => (
+  <li>
+    <button onClick={clickHandler} className="buttonToLink">
+      {name}
+    </button>
+  </li>
+);
 
 function MovieList({ allMovies, selectedMovie, movieClick }) {
   return (
@@ -10,23 +21,15 @@ function MovieList({ allMovies, selectedMovie, movieClick }) {
       <div className="MovieList">
         <ul>
           {allMovies.map((movie) => (
-            /*
-              I wanted the user to "feel" like they're being directed to individual movie's page, but under the hood React 
-              is conditionally rendering the movie view component, not directing the user somewhere.
-              
-              I used button's ability to perform an action, with CSS styling to make the button look like a link instead.
-              */
-            <button
-              key={movie.pk} //list items in React must have unique keys
-              className="buttonToLink"
-              onClick={(e) => {
+            <Movie
+              key={movie.pk}
+              clickHandler={function (e) {
                 e.preventDefault();
                 selectedMovie.current = movie;
                 movieClick();
               }}
-            >
-              <Movie name={movie.name} pk={movie.pk} />
-            </button>
+              name={movie.name}
+            />
           ))}
         </ul>
       </div>
